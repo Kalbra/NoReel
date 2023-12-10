e28082b6b78322e095ddd1ff4d01f5621c28db2a
kalle <kalle.bracht@outlook.de>
2023-12-10 20:48:57 +0100

    Rollback autouupdate

--- app/src/main/java/com/example/noreel/SettingsActivity.kt
+++ app/src/main/java/com/example/noreel/SettingsActivity.kt
@@ -1,13 +1,8 @@
 package com.example.noreel
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.dcastalia.localappupdate.DownloadApk
-import java.text.SimpleDateFormat
-import java.util.Date
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,21 +19,5 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
         requireActivity().title = "Settings"
-    }
-
-    override fun onPreferenceTreeClick(preference: Preference): Boolean {
-        if(preference.key.equals("autoupdater")){
-            val url = "https://github.com/Kalbra/NoReel/releases/latest/download/NoReel.apk"
-            val downloadApk = context?.let { DownloadApk(it) }
-            Log.d("Update", "Looking for updates")
-
-            downloadApk?.startDownloadingApk(url)
-
-            val sdf = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss")
-            val timestamp = sdf.format(Date())
-
-            downloadApk?.startDownloadingApk(url, timestamp)
-        }
-        return super.onPreferenceTreeClick(preference)
     }
 }
